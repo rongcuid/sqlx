29fd5397802077751b88fb74b130a5684ab25273
Rongcui Dong <rongcuid@outlook.com>
2022-07-18 09:16:48 -0400

    Some more functions

--- sqlx-core/src/sqlite/connection/explain.rs
+++ sqlx-core/src/sqlite/connection/explain.rs
@@ -607,7 +607,7 @@ pub(super) fn explain(
                     //assume that AGG_FINAL will be called
                     let p4 = from_utf8(p4).map_err(Error::protocol)?;
 
-                    if p4.starts_with("count(") || p4.starts_with("row_number(") {
+                    if p4.starts_with("count(") || p4.starts_with("row_number(") || p4.starts_with("rank(") || p4.starts_with("dense_rank(") || p4.starts_with("ntile("){
                         // count(_) -> INTEGER
                         state.r.insert(
                             p3,
@@ -625,7 +625,7 @@ pub(super) fn explain(
                 OP_AGG_FINAL => {
                     let p4 = from_utf8(p4).map_err(Error::protocol)?;
 
-                    if p4.starts_with("count(") || p4.starts_with("row_number(") {
+                    if p4.starts_with("count(") || p4.starts_with("row_number(") || p4.starts_with("rank(") || p4.starts_with("dense_rank(") || p4.starts_with("ntile(") {
                         // count(_) -> INTEGER
                         state.r.insert(
                             p1,
